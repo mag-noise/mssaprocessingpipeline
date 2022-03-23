32cb49dc3861e7d28f5208fdd17edd2949200d76
Steele <klsteele@uiowa.edu>
2022-03-23 17:38:11 -0500

    building out matrix calculations

--- src/MSSA/MSSA.cpp
+++ src/MSSA/MSSA.cpp
@@ -1,18 +1,39 @@
 #include "MSSA.hpp"
 #include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
 #include <string>
 #include <iostream>
 namespace Processor{
     using namespace std;
     using Eigen::MatrixXd;
 
-    void MSSA::Process(double input_signal1[], double input_signal2[]) {
-        const int k = input_size - window_size + 1;
-        MatrixXd m(window_size, k);
-        MatrixXd n(window_size, k);
+    // NOTE: Stack size on Windows: 1 MB - on Linux: 8 MB
+    void MSSA::Process(array<double, input_size> input_signal1, array<double, input_size> input_signal2) {
+        assert(
+            sizeof(input_signal1) / sizeof(input_signal1[0]) == input_size  
+            && sizeof(input_signal2) / sizeof(input_signal2[0]) == input_size
+        );
+        GenerateProjection(GenerateTrajectoryMatrix(input_signal1, input_signal2));
         
     }
 
-    MatrixXd generateMatrix
+    MSSA::TrajectoryMatrix MSSA::GenerateTrajectoryMatrix(array<double, input_size> &input_signal1, array<double, input_size> &input_signal2) {
+        TrajectoryMatrix t;
+        for (auto i = 0; i < window_size; i++) {
+            for (auto j = 0; j < k; j++) {
+                t(i, j) = input_signal1[i + j];
+                t(i+window_size, j) = input_signal2[i + j];
+            }
+        }
+        return t;
+    }
+
+    MSSA::ProjectionMatrix MSSA::GenerateProjection(TrajectoryMatrix trajectory)
+    {
+        TrajCovarianceMatrix c = trajectory*(trajectory.transpose())/k;
+        Eigen::EigenSolver<TrajCovarianceMatrix> solver(c);
+        return solver.eigenvectors().transpose()*trajectory;
+    }
+    
     
 }
--- src/MSSA/MSSA.hpp
+++ src/MSSA/MSSA.hpp
@@ -1,24 +1,32 @@
 #pragma once
 #include <string>
+#include <Eigen/Dense>
 namespace Processor{
     using namespace std;
     class MSSA{
-        public:
-            // Name: Process
-            // Description: take input signal stream filter out noise from system
-            // Input: multi-channel double array
-            // Output:
-            void static Process(double input_signal1[], double input_signal2[]);
         private:
             const static int input_size = 200;
             const static int window_size = 60;
+            const static int k = input_size - window_size + 1;
+            const static int number_of_signals = 2;
+            typedef Eigen::Matrix<double, window_size, k> SignalMatrix;
+            typedef Eigen::Matrix<double, window_size* number_of_signals, k> TrajectoryMatrix;
+            typedef Eigen::Matrix<double, window_size* number_of_signals, window_size* number_of_signals> TrajCovarianceMatrix;
+            typedef Eigen::Matrix<double, window_size* number_of_signals, window_size* number_of_signals> EigenVectorMatrix;
+            typedef Eigen::Matrix<double, window_size* number_of_signals, k> ProjectionMatrix;
+            typedef Eigen::Matrix<double, input_size, window_size* number_of_signals> ReconstructionMatrix;
+            typedef Eigen::Vector<double, window_size* number_of_signals + k - 1> SkewVector;
         // Separated units of work for debugging purposes
         // TODO: fill out implemention step-wise
-
-            void BuildTrajectory();
-            void MatrixDecomposition();
-            void Projection();
+            TrajectoryMatrix static GenerateTrajectoryMatrix(array<double, input_size> &input_signal1, array<double, input_size> &input_signal2);
+            ProjectionMatrix static GenerateProjection(TrajectoryMatrix trajectory);
             void SkewAverage();
             void Reconstruct();
+    public:
+        // Name: Process
+        // Description: take input signal stream filter out noise from system
+        // Input: multi-channel double array
+        // Output:
+        void static Process(array<double, input_size> input_signal1, array<double, input_size> input_signal2);
     };
 }
