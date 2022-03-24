611bb5c4fbcd6b27c1b13fe32c5944b38ecf2edd
Steele <klsteele@uiowa.edu>
2022-03-24 15:56:25 -0500

    added full MSSA pathways. now needs testing, fixing, and optimizations

--- src/MSSA/MSSA.cpp
+++ src/MSSA/MSSA.cpp
@@ -8,12 +8,12 @@ namespace Processor{
     using Eigen::MatrixXd;
 
     // NOTE: Stack size limit 128 KB
-    void MSSA::Process(array<double, input_size> input_signal1, array<double, input_size> input_signal2) {
+    MSSA::ReconstructionMatrix MSSA::Process(array<double, input_size> input_signal1, array<double, input_size> input_signal2) {
         assert(
             sizeof(input_signal1) / sizeof(input_signal1[0]) == input_size  
             && sizeof(input_signal2) / sizeof(input_signal2[0]) == input_size
         );
-        GenerateProjection(GenerateTrajectoryMatrix(input_signal1, input_signal2));
+        return GenerateProjection(GenerateTrajectoryMatrix(input_signal1, input_signal2));
         
     }
 
@@ -35,11 +35,11 @@ namespace Processor{
         return ReconstructMatrix((solver.eigenvectors().transpose())*trajectory, solver.eigenvectors());
     }
 
-    MSSA::SkewVector MSSA::SkewVectorAverage(ProjectionMatrix proj)
+    MSSA::SkewVector MSSA::SkewVectorAverage(Eigen::Matrix<double, window_size, k> proj)
     {
         SkewVector builder;
 
-        for (auto i = 0; i < window_size*2; i++) {
+        for (auto i = 0; i < window_size; i++) {
             for (auto j = 0; j < k; j++) {
                 builder[i + j] += proj(i, j)/(i+j+1);
             }
@@ -49,8 +49,28 @@ namespace Processor{
 
     MSSA::ReconstructionMatrix MSSA::ReconstructMatrix(ProjectionMatrix proj, EigenVectorMatrix eig)
     {
+        ReconstructionMatrix rMatrix;
+        Eigen::VectorXd testVector;
+        Eigen::VectorXd testVector2;
+        Eigen::MatrixXd endVector;
+        for (auto m = 0; m < window_size * number_of_signals; m++) {
+            for (auto sig_m = 0; sig_m < number_of_signals; sig_m++) {
+                // Reconstruction matrix of 1 input = NxLM, thus matrix created to hold NxLM^2 elements to allow an NxLM matrix for each M.
+                // Skew vector takes a matrix LxK constructed from an L vector crossed with K vector, 
+                // then generates an N vector of averages from the LxK skew diagonals
 
-        return ReconstructionMatrix();
+                // 40x1| 61x1
+                testVector = eig.col(m)(Eigen::seq(sig_m * window_size, (sig_m + 1) * window_size - 1));
+                testVector2 = proj.row(m);
+                endVector = testVector*(testVector2.transpose());
+                rMatrix.col(m + window_size * number_of_signals * sig_m) =
+                    SkewVectorAverage(endVector);
+                /*rMatrix.col(m + window_size*number_of_signals*sig_m) = 
+                    SkewVectorAverage(eig.col(m)(Eigen::seq(sig_m*window_size, (sig_m+1)*window_size - 1))*(proj.row(m).transpose()));*/
+            }
+            
+        }
+        return rMatrix;
     }
 
     
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,25 @@
 #include "MSSA/MSSA.hpp"
 #include <Eigen/Dense>
 #include <iostream>
+#include <fstream>
 #include <string>
-
 
 int main() {
     using namespace std;
     using Eigen::MatrixXd;
-    Processor::MSSA::Process({}, {});
+    std::array<double, 100> input1;
+    std::array<double, 100> input2;
+    for_each(input1.begin(), input1.end(), [](double& n) { n = (rand() % 5); });
+    for_each(input2.begin(), input2.end(), [](double& n) { n = (rand() % 5); });
+    
+    
+    string path = "log.csv";
+    ofstream ofs(path);
+    if (!ofs)
+        return -1;
+
+    // CSV Formatting example: https://stackoverflow.com/questions/61987600/write-eigen-vectorxd-in-csv-format
+    const static Eigen::IOFormat CSVFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", ", ");
+    ofs << Processor::MSSA::Process(input1, input2).format(CSVFormat);
     return 0;
 }
