898b11a02990ef43f3e3ae78c35afc0b155c8d8f
Steele <klsteele@uiowa.edu>
2022-03-30 12:05:04 -0500

    working testing for preprocessor

--- src/PreProcessing/PProcessor.hpp
+++ src/PreProcessing/PProcessor.hpp
@@ -1,13 +1,36 @@
 #pragma once
-#include <string>
 #include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+
 namespace PreProcessing{
 	using namespace std;
-	class PreProcessor{
+	template < typename T >
+	class MSSAPreProcessor {
 		public:
 			// TODO: update to cleanly check that only container types are used
-			template<typename Container>
-			void static CreateValidSegment(string&, Container const *);
+			void static CreateValidSegment(string& input_file, vector<T>& output_container);
 	};
-	
+
+	template<class InputType>
+	void MSSAPreProcessor<InputType>::CreateValidSegment(string& input_file, vector<InputType>& output_container)
+	{
+		ifstream file{ input_file };
+		string line;
+		InputType val;
+		if (file.is_open()) {
+			while (getline(file, line)) {
+				std::stringstream line_stream(line);
+				while (line_stream >> val) {
+					output_container.push_back(val);
+					if (line_stream.peek() == ',') line_stream.ignore();
+				}
+			}
+		}
+
+	}
+
 }
---
+++ tests/TestPreProcessor.cpp
@@ -0,0 +1,48 @@
+#include "../src/MSSA/MSSA.hpp"
+#include "../src/PreProcessing/PProcessor.hpp"
+
+#include "gtest/gtest.h"
+#include <Eigen/Dense>
+#include <cmath>
+#include <numeric>
+#include <string>
+#include <vector>
+
+namespace Testing {
+    namespace PreProcessingTest {
+        namespace {
+
+            class PreProcessorTest : public ::testing::Test {
+            public:
+                std::string test_csv;
+            protected:
+
+                // You can remove any or all of the following functions if their bodies would
+                // be empty.
+
+                PreProcessorTest() {
+                    // You can do set-up work for each test here.
+                    test_csv = "C:\\Users\\klsteele\\source\\repos\\mssaprocessingpipeline\\data\\signal1.csv";
+                }
+
+                ~PreProcessorTest() override {
+                    // You can do clean-up work that doesn't throw exceptions here.
+                }
+
+            };
+
+
+            TEST_F(PreProcessorTest, CorrectlyReadsCSVFile) {
+                using PreProcessing::MSSAPreProcessor;
+                using Processor::MSSA;
+                using namespace std;
+                // TODO: create test to add input_size to some array/vector and have all values be valid floating point values
+                vector<float> test_arr = {};
+                MSSAPreProcessor<float>::CreateValidSegment(test_csv, test_arr);
+                
+                EXPECT_FLOAT_EQ(test_arr[0], 4);
+            }
+
+        }  // namespace
+    }  // namespace project
+}  // namespace my
