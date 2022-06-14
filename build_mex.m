ef66cc20d7dc68a71db5e0a79c9131dcb6396cfa
Steele <klsteele@uiowa.edu>
2022-06-14 14:38:14 -0500

    small changes to building

--- build_mex.m
+++ build_mex.m
@@ -1,9 +1,9 @@
 function build_mex(vcpkg_dir)
     if(~exist('vcpkg_dir', 'var'))
-        vcpkg_dir = fullfile(pwd, 'tools', 'vcpkg');
+        vcpkg_dir = fullfile(pwd, 'tools', 'vcpkg', 'installed', 'x64-windows' );
     end
-    ipath = ['-I' fullfile(vcpkg_dir, 'installed', 'x64-windows', 'include')];
-    lpath = ['-I' fullfile(pwd, 'src')];
+    ipath = "-I"+ fullfile(vcpkg_dir, 'include');
+    lpath = "-I"+ fullfile(pwd, 'src');
     mex(ipath, lpath, 'src/mexEntry.cpp', 'src/MSSA/MSSA.cpp', 'src/SPU/SPU.cpp');
     
     %ipath = ['-I' fullfile(pwd, 'tools', 'vcpkg', 'installed', 'x64-windows', 'include')];
