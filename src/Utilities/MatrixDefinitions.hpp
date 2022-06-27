c38859e0fe961c7d6e27c944b5637417d106daa5
Steele <klsteele@uiowa.edu>
2022-06-27 14:35:39 -0500

    fix to nan and time skip tracking

--- src/Utilities/MatrixDefinitions.hpp
+++ src/Utilities/MatrixDefinitions.hpp
@@ -102,7 +102,7 @@ namespace Utils{
 
 
             for (auto i = 1; i < timeseries.size(); i++)
-                instance->flags[i].time_jump = (timeseries[i]) > mean;
+                instance->flags[i].time_jump |= (timeseries[i]) > mean;
         }
         
         /// <summary>
@@ -115,18 +115,17 @@ namespace Utils{
         /// <returns></returns>
         void FindFlagInSegment(std::size_t start_idx, std::size_t segment_size, std::vector<int>& idx_vector, bool valid_past=false) {
             // Logic for recurrive overflow
-            if(start_idx >= Size() || (start_idx + segment_size) > Size())
+            if(start_idx >= Size() || (!valid_past && (start_idx + segment_size) > Size()))
                 return;
 
             // Logic for last segment not fitting size requirements
             if (valid_past && (start_idx + segment_size) > Size()) {
                 idx_vector.push_back(Size() - segment_size);
-                
+
                 // Flag merge needed for elements in the last segment
-                std::for_each(instance->flags.begin() + Size() - segment_size, instance->flags.begin() + start_idx, [](flag& ele) {ele.merge_required = 1; });
+                std::for_each(instance->flags.begin() + Size() - segment_size, instance->flags.begin() + start_idx, [](flag& ele) {ele.merge_required |= 1; });
                 return;
             }
-
 
             std::vector<flag> unraised = { flag() };
             
@@ -145,16 +144,20 @@ namespace Utils{
                 // +1 accounts for moving the flagged value past the flagged instance. However, if the flag is a time jump, we are allowed to worked on it as a starting point
                 int y = int(last - (instance->flags.begin())+1*(!instance->flags[last - (instance->flags.begin())].TimeJumpOnly()));
                 if (y == last - (instance->flags.begin()))
-                    instance->flags[y].time_jump_used = 1;
+                {
+                    instance->flags[y].time_jump_used |= 1;
+                    FindFlagInSegment(y, segment_size, idx_vector, false);
+                    return;
+                }
 
                 
                 // If availabe space before starting index, utilize values prior to the first flagged instance
                 if (valid_past) {
                     idx_vector.push_back(int(first - (instance->flags.begin())) - segment_size);
-                    std::for_each(first - segment_size, instance->flags.begin()+start_idx, [](flag& ele) {ele.merge_required = 1; });
+                    std::for_each(first - segment_size, instance->flags.begin()+start_idx, [](flag& ele) {ele.merge_required |= 1; });
                 }
                 else {
-                    std::for_each(instance->flags.begin() + start_idx, first+1, [](flag& ele) { ele.skipped_value = 1; });
+                    std::for_each(instance->flags.begin() + start_idx, first+1, [](flag& ele) { ele.skipped_value |= 1; });
                 }
                 FindFlagInSegment(y, segment_size, idx_vector, false);
                 //bool pick_idx = (y - start_idx == segment_size);
